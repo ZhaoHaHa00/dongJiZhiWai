165cac72b292d0ec4b4ddb1208753ca6e77ecc2a
ZhaoHaHa00 <zhao2277910235@163.com>
2023-06-28 15:08:37 +0800

    登录接口

--- src/main/java/com/tencent/wxcloudrun/dao/AdminMapper.java
+++ src/main/java/com/tencent/wxcloudrun/dao/AdminMapper.java
@@ -3,9 +3,10 @@ package com.tencent.wxcloudrun.dao;
 import com.tencent.wxcloudrun.dto.LoginDTO;
 import com.tencent.wxcloudrun.model.Admin;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface AdminMapper {
 
-    Admin getAdminInfo(LoginDTO loginDTO);
+    Admin getAdminInfo(@Param("loginDTO") LoginDTO loginDTO);
 }
