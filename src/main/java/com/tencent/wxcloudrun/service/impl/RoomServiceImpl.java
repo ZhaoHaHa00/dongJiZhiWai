cfd0cec213ac1af24244370125ea03221f702278
ZhaoHaHa00 <zhao2277910235@163.com>
2023-06-30 13:14:29 +0800

    房间创建时间返给前端字符串

--- src/main/java/com/tencent/wxcloudrun/service/impl/RoomServiceImpl.java
+++ src/main/java/com/tencent/wxcloudrun/service/impl/RoomServiceImpl.java
@@ -4,9 +4,12 @@ import com.tencent.wxcloudrun.config.ApiResponse;
 import com.tencent.wxcloudrun.dao.RoomMapper;
 import com.tencent.wxcloudrun.model.Room;
 import com.tencent.wxcloudrun.service.RoomService;
+import com.tencent.wxcloudrun.vo.RoomVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -18,6 +21,14 @@ public class RoomServiceImpl implements RoomService {
     @Override
     public ApiResponse getAllRoom(String validNum) {
         List<Room> roomList = roomMapper.getAllRoom(validNum);
-        return ApiResponse.ok(roomList);
+        List<RoomVO> resList = new ArrayList<>();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        for (Room room : roomList) {
+            RoomVO roomVO = new RoomVO();
+            roomVO.setRoomNum(room.getRoomNum());
+            roomVO.setCreatedTime(sdf.format(room.getCreatedTime()));
+            resList.add(roomVO);
+        }
+        return ApiResponse.ok(resList);
     }
 }
---
+++ src/main/java/com/tencent/wxcloudrun/vo/RoomVO.java
@@ -0,0 +1,11 @@
+package com.tencent.wxcloudrun.vo;
+
+import lombok.Data;
+
+@Data
+public class RoomVO {
+
+    private String roomNum;
+
+    private String createdTime;
+}
